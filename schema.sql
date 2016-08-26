59efc32e2d76788e1f713631a4aa6167be5a6aa8
root <root@localhost.localdomain>
2016-08-26 13:48:48 +0900

    データベーススキーマの追加
    データベーススキーマを追加しました

---
+++ schema.sql
@@ -0,0 +1,18 @@
+create database choseikun;
+
+
+CREATE TABLE schedule(
+	schedule_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	event_url VARCHAR(128) NOT NULL,
+	candidate_schedule TEXT ,
+	display_name TEXT,
+	evaluation INTEGER NOT NULL
+);
+
+CREATE TABLE event(
+	event_url VARCHAR(128) PRIMARY KEY,
+	event_name TEXT NOT NULL,
+	memo TEXT,
+	candidate_schedule TEXT NOT NULL,
+	registeration_date DATE NOT NULL
+);
