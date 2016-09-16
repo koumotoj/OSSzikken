4644217cc5b010f2da9af1d746d6cb712e6677e1
xkw19900215 <s15023@st.kic.ac.jp>
2016-09-16 16:40:44 +0900

    9月16日更新

--- controller/ScheduleAddController.php
+++ controller/ScheduleAddController.php
@@ -1,19 +1,25 @@
 <?php
 
-  require_once'../view/Schedule.php';
-  require_once'../model/ScheduleDAO.php';
+  require_once'/var/www/html/OSSzikken/model/EventDAO.php';
+  require_once'/var/www/html/OSSzikken/model/Event.php';
+  require_once'/var/www/html/OSSzikken/model/ScheduleDAO.php';
+  require_once'/var/www/html/OSSzikken/model/Schedule.php';
+  
+  $schedule_name = $_POST['name'];
+  $schedule_evaluation = $_POST['eva'];
+  $schedule_comment = $_POST['com'];
 
-  $schedule_name = $_POST[''];
-  $schedule_evaluation = $_POST[''];
-  $schedule_comment = $_POST[''];
+  session_start();
+  $hash = $_SESSION['url'];
 
-  $hash = $_GET['hash'];
+  //$hash = $_GET['hash'];
+  $event = new Event();
+  $candidateschedule = $event -> getCandidateschedule();
+
   $scheduleDao = new ScheduleDAO();
-  $newschedule = $scheduleDao -> getschedule($hash);
+  $scheduleDao -> setSchedule($hash,$candidateschedule,$schedule_name,$schedule_evaluation);	
 
-  $newscheduleid = count(scheduleId());
-  $newschedule = new Schedule($newscheduleid,$eventUrl,$candidateSchedule,$schedule_name,$schedule_evaluation,$schedule_comment);
-  array_push($scheduleArray,$newschedule);
+  //$scheduleDao -> setSchedule($hash,$candidateschedule,$schedule_name,$schedule_evaluation,$schedule_comment);	
 
   header('Location: ../view/Schedule.php');
   exit();
