3f2f2c661190ba312f2a2683c92415295296806d
root <root@T.M>
2016-09-28 13:44:35 +0900

    error

---
+++ view/error.php
@@ -0,0 +1,33 @@
+<!--
+author：Tomoya Morimoto
+date：2016/09/28
+comment:
+エラー画面
+-->
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>エラー！！</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css_origin/pub.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+</head>
+<body>
+<br>
+ <div class="container">
+	
+  <div class="box1">
+   <h1>調整くん</h1>
+  </div>
+  
+  <h1>エラーです！</h1>
+  
+  <input type="button" onclick="location.href='./index.php'" value="トップページへ戻る" />
+</body>
+</html>
